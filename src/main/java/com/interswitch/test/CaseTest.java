8f470e40bf4566c4ed50ee2c30b94dca03cb0e85
Tim 'Laja <adelajatimothy@gmail.com>
2023-08-18 16:10:04 +0100

    Go home print out

--- src/main/java/com/interswitch/test/CaseTest.java
+++ src/main/java/com/interswitch/test/CaseTest.java
@@ -1,10 +1,15 @@
 package com.interswitch.test;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class CaseTest {
     public static void main(String[] args) {
         UpperLowerCase ulcase = new UpperLowerCase();
         System.out.printf("%s to %s", "Hello World", ulcase.wordToUpperCase("Hello World"));
         System.out.println();
         System.out.printf("%s to %s\n","Hello World", ulcase.wordToLowerCase("Hello World"));
+
+        System.out.println("New Go-home feature by Backbone Team  at " + LocalDateTime.now());
     }
 }
